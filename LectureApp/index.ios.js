f70bb911d5f140add93c11b64837c97279957926
Brian McMillan <brianm_91@hotmail.com>
2016-11-02 14:48:39 -0400

    Uploading basic adventure mode

---
+++ LectureApp/index.ios.js
@@ -0,0 +1,54 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class LectureApp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#658E9C',
+  },
+  welcome: {
+    fontSize: 30,
+    textAlign: 'center',
+    margin: 10,
+    color: '#FFFFFF',
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#FFFFFF',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('LectureApp', () => LectureApp);
