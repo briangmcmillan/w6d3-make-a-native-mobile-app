b5e76ddf5bdc13e54b744e20f66ccbfc7ab30578
Brian McMillan <brianm_91@hotmail.com>
2016-11-02 22:14:22 -0400

    Uploading 3 components

--- LectureApp/index.ios.js
+++ LectureApp/index.ios.js
@@ -1,31 +1,76 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+* Sample React Native App
+* https://github.com/facebook/react-native
+* @flow
+*/
 
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  TextInput,
+  Modal,
+  TouchableHighlight,
 } from 'react-native';
 
 export default class LectureApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      modalVisible: false
+    };
+  }
+  setModalVisible(visible) {
+      this.setState({modalVisible: visible});
+    }
   render() {
+    let pic = {
+      uri: 'https://source.unsplash.com/random'
+    };
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+        Welcome!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+        Check out my Hacked together components!
         </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Image source={pic} style={{borderRadius: 80, width: 150, height: 150}}/>
+        <TextInput selectionColor= 'white'
+        style={{textAlign: 'center', height: 30, borderColor: 'white', borderWidth: .5, color: '#FFFFFF',}}
+        placeholder="What are your thoughts..."
+        onChangeText={(text) => this.setState({text})}
+        value={this.state.text}
+        />
+        <Modal
+          animationType={"slide"}
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {alert("Modal has been closed.")}}
+          >
+         <View style={{marginTop: 22}}>
+          <View>
+            <Text>Hello World!</Text>
+
+            <TouchableHighlight onPress={() => {
+              this.setModalVisible(!this.state.modalVisible)
+            }}>
+              <Text>Hide Modal</Text>
+            </TouchableHighlight>
+
+          </View>
+         </View>
+        </Modal>
+
+        <TouchableHighlight onPress={() => {
+          this.setModalVisible(true)
+        }}>
+          <Text style={styles.instructions}>My Modal</Text>
+        </TouchableHighlight>
       </View>
     );
   }
